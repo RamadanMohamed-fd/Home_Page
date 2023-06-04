84cdc1c2d97077e3d2baf25f54561be77771cab7
Ramadan Mohamed <81324838+RamadanMohamed-fd@users.noreply.github.com>
2023-06-05 02:03:19 +0300

    Update MenuItems.js

--- src/components/Navbar/MiddleMenu/MenuItems.js
+++ src/components/Navbar/MiddleMenu/MenuItems.js
@@ -14,13 +14,13 @@ const MenuItems = ({ menuItemsData,index }) => {
       <ul className={`${open?"z-[201]  block translate-x-0":"translate-x-[-20px] hidden  z-[-1]"}  absolute font-normal  bg-gray-50   p-3 w-fit px-1  rounded-md mt-2 ml-0  text-[14px] duration-700  ` } >
         {
          sub[index].map((data, i) => (
-            <li key={i} className="py-2 px-4 underline group/i hover:no-underline duration-200 hover:text-[var(--color-default)]">
+            <li key={i} className="py-2 px-4  group/i  duration-200 hover:text-[var(--color-default)]">
               <a className="flex justify-between  relative items-center" href={data.url}>{data.title} {data["sup"]&&<MdKeyboardArrowRight className=" text-lg group-hover/i:rotate-90 duration-200" />}  </a>
               {data["sup"]&&
                 <ul className={`z-[201] absolute  bg-gray-50 p-4 w-fit left-[90%]  rounded-md mt-2 ml-0 hidden top-[60%] group-hover/i:block text-[14px] duration-1000 ` }>
                    {
                     data["sup"].map((s,index)=>(
-                      <li key={index} className=" cursor-pointer py-2 underline text-slate-700 hover:text-[var(--color-default)] hover:no-underline"><a className="flex justify-between relative items-center" href={s.url}>{s.title}</a></li>
+                      <li key={index} className=" cursor-pointer py-2  text-slate-700 hover:text-[var(--color-default)] "><a className="flex justify-between relative items-center" href={s.url}>{s.title}</a></li>
                     ))
                    }
                 </ul>
