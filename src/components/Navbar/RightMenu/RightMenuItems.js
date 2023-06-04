97e16754b1d00e921eae6a35bae213e3bfda5150
Ramadan Mohamed <81324838+RamadanMohamed-fd@users.noreply.github.com>
2023-06-05 01:59:56 +0300

    Update RightMenuItems.js

--- src/components/Navbar/RightMenu/RightMenuItems.js
+++ src/components/Navbar/RightMenu/RightMenuItems.js
@@ -16,7 +16,7 @@ const RightMenuItems = ({ data,index }) => {
         {
           sub[index].map((item,i)=>(
             <li key={i} className={` ml-6 text-gray-700 pt-1.5 duration-200 hover:text-[var(--color-default)]
-             font-medium underline hover:no-underline`}>{item.title}</li>
+             font-medium `}>{item.title}</li>
           ))
         }
       </ul>}
@@ -25,37 +25,3 @@ const RightMenuItems = ({ data,index }) => {
 };
 
 export default RightMenuItems;
-
-
-// import React, { useState } from 'react'
-
-// const RightMenuItems = ({menuItemsData}) => {
-//   const [activeItem, setActiveItem] = useState(null);
-
-//   const handleItemClick = (item) => {
-//     if (activeItem === item) {
-//       setActiveItem(null);
-//     } else {
-//       setActiveItem(item);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <ul>
-//         {menuItemsData.map((item, index) => (
-//           <li key={index}>
-//             {/* <a href={item.href} > */}
-//               <h3 onClick={() => handleItemClick(item)}>{item.title}</h3>
-//             {/* </a> */}
-//             {activeItem === item && item.subMenuItems && (
-//               <RightMenuItems menuItemsData={item.subMenuItems} />
-//             )}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   )
-// }
-
-// export default RightMenuItems
